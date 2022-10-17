8d7d1c97d626a78b270b302b65746817a59334e2
moazk17 <102334207+moazk17@users.noreply.github.com>
2022-10-16 21:28:53 -0400

    Update grid.c

--- grid.c
+++ grid.c
@@ -1,17 +1,3 @@
-///////////////////////////////////////////////////////////////////////////// 
-// INTEGRITY STATEMENT (v3)
-//
-// By signing your name and ID below you are stating that you have agreed
-// to the online academic integrity statement:a
-// https://student.cs.uwaterloo.ca/~cs136/current/assignments/integrity.shtml
-/////////////////////////////////////////////////////////////////////////////
-// I received help from and/or collaborated with: 
- 
-// 0 
-//  
-// Name: Moaz Mahran 
-// login ID: mkgmahra 
-//////////////////////////
 
 #include "grid.h"
 #include <stdbool.h>
